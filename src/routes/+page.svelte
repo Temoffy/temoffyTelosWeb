be3f93249af6d42fffd59089052aa96950152c1e
Telos <Temoffy@gmail.com>
2026-04-25 13:00:09 -0500

    initialize

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<h1>Welcome Visitors!</h1>
+
+<p>Unfortunately, you've reached a site that doesn't exist yet. try again later maybe! Setting up a webpage as a porfolio/project display/alternate to social apps seemed handy, may migrate some of my stuff here if even for my own reference</p>
+
+<p>I'm going to use this as a note page because I'm doing this away from my main workstation and want to remember some links and such, don't mind me! (this is how indie web is supposed to work anyhow right? link to lots of things? :P)</p>
+
+<!--<img src="/neocities.png" alt="Site hosted by Neocities">-->
+
+<ul>
+    <li>Someone already made a great <a href="https://neocities.org">dithering tool</a>, and it works in Oklab!!! That makes my pet color handling project Chromaloom easier. If you want to see Chromaloom actually hit the web yell at me and i might actually get around to it</li>
+    <li>stumbled across <a href="https://midnightsolarium.neocities.org/links">this neocities site</a> while googling neocities vs nekocities, very handy stuff here.</li>
+    <li>One of the things I discovered through the above is the <a href="https://marginalia-search.com/">marginalia search engine</a> which I would normally ignore, BUT I decided to test it by searching for the old 4x game Stars! (yes, including exclamation) and it POPPED UP IN THE SUGGESTED SEARCHES LIST AS "stars! autohost"!! I may not use this a lot, but for obscure or older topic I might bring it to bear.</li>
+</ul>
+
+<h2>Update</h2>
+<p>this website is now built on sveltekit <br> That should make it very easy to maintain fancier pieces!</p>
